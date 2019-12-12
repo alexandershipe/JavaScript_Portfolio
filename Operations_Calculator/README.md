bf9e727ed722d516bbfdc608c3791f7d13c09a14
alexandershipe <42188215+alexandershipe@users.noreply.github.com>
2019-12-12 01:57:17 -0500

    Added Operations Calculator
    Added html, js, css, and README file.

---
+++ Operations_Calculator/README.md
@@ -0,0 +1,6 @@
+This is a basic operations calculator using HTML, JavaScript, and CSS
+
+The user can choose one of four operations: Addition, Subtraction, Multiplication, and Division.
+
+The user can set two operands in the operand 1 and operand 2 field.
+Note: Operand 1 will be divided by operand 2 for the division operation.
---
+++ Operations_Calculator/script.js
@@ -0,0 +1,61 @@
+function calculateme(){
+
+   // var operation = document.getElementById("operation").value;
+
+    if (document.getElementById("operation1").checked)
+    {
+        operation = document.getElementById("operation1").value;
+    }
+    if (document.getElementById("operation2").checked)
+    {
+        operation = document.getElementById("operation2").value;
+    }
+    if (document.getElementById("operation3").checked)
+    {
+        operation = document.getElementById("operation3").value;
+    }
+    if (document.getElementById("operation4").checked)
+    {
+        operation = document.getElementById("operation4").value;
+    }
+
+    if(isNaN(document.getElementById("operand1").value)){
+        return window.alert("Please enter a valid number for operand 1."); 
+    }
+    else if(isNaN(document.getElementById("operand2").value)){
+        return window.alert("Please enter a valid number for operand 2."); 
+    }
+
+    var operand1  = parseInt(document.getElementById("operand1").value, 10);
+    var operand2  = parseInt(document.getElementById("operand2").value,10);
+
+    var answer = 7.0;
+
+    if (operation == "Addition")
+    {
+        answer = operand1 + operand2;
+    }
+    else if (operation == "Subtraction")
+    {
+        answer = operand1 - operand2;
+    }
+    else if (operation == "Multiplication")
+    {
+        answer = operand1*operand2;
+    }
+    else 
+    {
+        answer = operand1/operand2;
+    }
+
+    document.getElementById("Answer").style.display = "block";
+    document.getElementById("final").innerHTML = answer;
+
+}
+
+
+// Hide the total amount on initial load
+document.getElementById("Answer").style.display = "none";
+
+// clicking button calls the Button click function
+document.getElementById("calculate").onclick = function() {calculateme();};
